c228cbebf4c23c43fe2663d1448377cac577f521
Nishit Majithia <nishitm@cse.iitk.ac.in>
2017-01-26 11:00:37 +0530

    proxy server for ftp

---
+++ ftp_server.py
@@ -0,0 +1,46 @@
+import os
+import logging
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+def main():
+
+	authorizer = DummyAuthorizer()
+
+	authorizer.add_user('user','12345','.',perm='elradfmwM')
+	authorizer.add_user('system','system','.',perm='elradfmwM')
+	authorizer.add_user('ubuntu','ubuntu','.',perm='elradfmwM')
+	authorizer.add_user('admin','admin','.',perm='elradfmwM')
+	authorizer.add_anonymous(os.getcwd())
+
+	handler = FTPHandler
+	handler.authorizer = authorizer
+	
+	f= open('./banner.dat')
+	s=""
+	for line in f:
+		s=s+line
+	
+	f.close()
+	    
+	handler.banner = s
+	
+	#print "---->",handler.rip
+	#ip = %(remote_ip)s + '.log'
+	#open(ip,'a+')
+	f11=open('./logs/ftp.log','w')
+	f11.write("#########################################################################################\n")
+	f11.close()
+	logging.basicConfig(filename='./logs/ftp.log', level=logging.INFO)
+	handler.log_prefix = '[%(username)s]@%(remote_ip)s'
+	address = ('',21)
+	server = FTPServer(address, handler)
+	server.max_cons = 256
+	server.max_cons_per_ip = 1
+	
+	server.serve_forever()
+	
+
+if __name__ == '__main__':
+	main()
